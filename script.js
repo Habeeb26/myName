426c651e0207594206f8a9ac4c58b80462ce1e57
Habeeb26 <105178817+Habeeb26@users.noreply.github.com>
2022-06-24 21:26:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let myName = 'Habeeb Odunsi';
+let myHeight= '6ft5inches';
+let myCountry= 'Nigeria';
+
+console.log('Name:'+myName);
+console.log('Height:'+myHeight);
+console.log('Country:'+myCountry);
+
